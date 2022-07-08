a0fc745c201baeac9b620fc1a161f5e6e072760a
Sergey Tikhov <tempeltru@gmail.com>
2022-07-08 19:45:18 +0500

    fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,20 +1,23 @@
 package web.controller;
 
 import models.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarService;
+import web.service.CarService;
 
 import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
+    private CarService carService;
     @GetMapping(value="/cars")
     public String carTable(@RequestParam(value = "count",required = false,defaultValue = "5") int count,
                            ModelMap model){
-        CarService carService = new CarService();
+        if(Double.isNaN((double)count)) count = 5;
         List<Car> cars = carService.getCarList(count);
         model.addAttribute("carTable",cars);
         return "cars";
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,30 +1,37 @@
-package service;
+package web.service;
 
 import models.Car;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class CarService {
+@Component
+public class CarService{
     private List<Car> carList = initList();
+
+
+
     private List<Car> initList(){
         carList = new ArrayList<>();
         carList.add(new Car(111,"Ford","Escape"));
         carList.add(new Car(148,"Chevrolet","Impala"));
-        carList.add(new Car(111,"Ford","Focus III"));
-        carList.add(new Car(111,"Opel","Astra"));
-        carList.add(new Car(111,"Audi","A4"));
+        carList.add(new Car(215,"Ford","Focus III"));
+        carList.add(new Car(16,"Opel","Astra"));
+        carList.add(new Car(77,"Audi","A4"));
         return carList;
     }
 
     public List<Car> getCarList(int count) {
         List<Car> output = new ArrayList<>();
+
         for(int i=0;i<count;i++){
-            if(count>=5){
+            //int не может быть равен null метод выкинет исключение;
+            if(count<5&&count>=1){
+                output.add(carList.get(i));
+            }else{
                 output = carList;
                 break;
             }
-            output.add(carList.get(i));
         }
         return output;
     }
